de6f7750fac26a2d30ee6bd88a4eed7f5bdb2a93
rahul.suthar <rahulsuthar1329@gmail.com>
2024-05-16 00:51:18 +0530

    env file updated

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ CreateAllFolder();
 
 // Database Connection
 mongoose
-  .connect(process.env.DATABASE, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
